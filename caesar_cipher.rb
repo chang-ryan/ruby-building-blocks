c8d00aa755b08e8acf13495e9a2db0327a015583
chang-ryan <chang-ryan@users.noreply.github.com>
2015-10-21 15:47:23 -0700

    Create caesar_cipher.rb

---
+++ caesar_cipher.rb
@@ -0,0 +1,29 @@
+def cipher(input, shift)
+    
+    num_array = []
+    letter_array = []
+    
+    input.downcase.each_byte do |x|
+        num_array << x
+    end
+    
+    num_array.each_index do |y|
+        if num_array[y] >= 97 and num_array[y] <= 122
+            if num_array[y] + shift > 122
+                num_array[y] = num_array[y] + shift - 26
+            else 
+                num_array[y] = num_array[y] + shift
+            end
+        end
+    end
+        
+    num_array.each do |z|
+        letter_array << z.chr
+    end
+    
+    letter_array.join
+end
+
+cipher("What a string!", 5)
+
+# A-Z 65-90, a-z 97-122
