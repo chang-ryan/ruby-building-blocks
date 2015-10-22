1dacac2d362060aa7c99bd82cc5071fb8cacb3b0
chang-ryan <chang-ryan@users.noreply.github.com>
2015-10-22 16:01:06 -0700

    Start project

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,1 +1,8 @@
+# accepts an array of prices corresponding to a day starting at 0
+# returns an array [buy, sell] that indicates which day to buy and which to sell
 
+def stock_picker(price)
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+# => [1,4]
