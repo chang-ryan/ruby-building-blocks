b587de3222e40d6977696350357a3465502f0d0e
chang-ryan <chang-ryan@users.noreply.github.com>
2015-10-22 19:49:34 -0700

    Add first workable method

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,7 +1,25 @@
 # accepts an array of prices corresponding to a day starting at 0
 # returns an array [buy, sell] that indicates which day to buy and which to sell
 
-def stock_picker(price)
+def stock_picker(prices)
+
+    result = []
+    profits = []
+    buy = 0
+    sell = 0
+    max = 0
+    
+    prices.each_index do |x|
+        prices.each_index do |y|
+            if prices[y] - prices[x] > 0 and prices[y] - prices[x] > max and y > x
+                buy = x
+                sell = y
+                max = prices[y] - prices[x]
+            end
+        end
+    end
+    
+    [buy, sell]
 end
 
 #        DAY  0  1 2 3 4  5 6 7 8
