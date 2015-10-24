09ac8b5c25536010fac7ea96aed345ddd84a9d19
chang-ryan <chang-ryan@users.noreply.github.com>
2015-10-23 18:29:30 -0700

    Add bubble_sort_by that accepts a block

--- advanced/bubblesort.rb
+++ advanced/bubblesort.rb
@@ -14,5 +14,25 @@ def bubblesort(numbers)
     numbers
 end
 
-bubblesort([99,4,3,78,2,0,2])
+p bubblesort([99,4,3,78,2,0,2])
 # => [0,2,2,3,4,78,99]
+
+def bubble_sort_by(list)
+    swapped = true
+    
+    while swapped
+    swapped = false
+        for i in (0...list.size-1)
+            if yield(list[i], list[i+1]) > 0
+                list.insert(i, list.delete_at(i+1))
+                swapped = true
+            end
+        end
+    end
+    list
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
+# => ["hi", "hey", "hello"]
