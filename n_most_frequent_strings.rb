43171415f1cfb3465d96365211414f260df55bcf
Ryan Chang <chang-ryan@users.noreply.github.com>
2016-06-07 13:48:50 -0700

    Update n_most_frequent_strings.rb

--- n_most_frequent_strings.rb
+++ n_most_frequent_strings.rb
@@ -1,11 +1,7 @@
 array = ["a","a","c","b","b","b","d"]
 
 def n_most_frequent_string(array, n)
-  result = []
-
-  array.uniq.each do |str|
-  	result = array.inject(Hash.new(0)) { |hash, str| hash[str] += 1; hash }.sort_by { |k,v| -v }
-  end
+  result = array.inject(Hash.new(0)) { |hash, str| hash[str] += 1; hash }.sort_by { |k,v| -v }
   
   result[0...n]
 end
