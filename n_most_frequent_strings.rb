9b8166430b8979f176c080d6bba331d4c1c39006
Ryan Chang <ry4nch4ng@gmail.com>
2016-09-01 12:05:58 -0700

    fix spacing

--- n_most_frequent_strings.rb
+++ n_most_frequent_strings.rb
@@ -2,7 +2,7 @@ array = ["a","a","c","b","b","b","d"]
 
 def n_most_frequent_string(array, n)
   result = array.inject(Hash.new(0)) { |hash, str| hash[str] += 1; hash }.sort_by { |k,v| -v }
-  
+
   result[0...n]
 end
 
